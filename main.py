858c5fcdb467f760c069724193b9644bb01374b6
Pegoku <pegoku08@gmail.com>
2025-05-03 12:08:12 +0200

    sort month's spendings

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from google.genai import types
 import os
 import pathlib
 from dotenv import load_dotenv
-import csv, io
+import csv
+import io
 
 load_dotenv()
 
@@ -41,19 +42,40 @@ response = client.models.generate_content(
     ))
 
 # Clean CSV
-response.text = "\n".join(
+cleaned_response_1 = "\n".join(
     line for line in response.text.splitlines() if '`' not in line)
-response.text = "\n".join(response.text.splitlines()[1:])
+cleaned_response = "\n".join(cleaned_response_1.splitlines()[1:])
 
+# No need to write the data to a file
+# with open("geminiOutput.csv", "w") as output_file:
+#     output_file.write(cleaned_response)
 
-with open("geminiOutput.csv", "w") as output_file:
-    output_file.write(response.text)
-
-stream = io.StringIO(response.text)
+# To lists
+stream = io.StringIO(cleaned_response)
 
 reader = csv.reader(stream)
 
 header = next(reader)
 data = list(reader)
 
+# Split into month groups && convert to nums
+data_month_separated = []
+for j in range(1, 13):
+    temp_list = []
+    for i in data:
+        i[0] = int(i[0])
+        i[2] = float(i[2])
+        i[3] = float(i[3])
+        i[4] = float(i[4])
+        if i[0] == j:
+            temp_list.append(i)
+    print(temp_list)
+    data_month_separated.append(temp_list)
 
+# Sort each month's spendings in descending order
+data_sorted = []
+for i in data_month_separated:
+    sorted_list = sorted(i, key=lambda x: x[2], reverse=True)
+    data_sorted.append(sorted_list)
+
+print(data_sorted)
